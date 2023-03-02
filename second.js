be8e9805218dceddf56686741e9ebd0ff4c46b40
Thana26 <makthalthana086@gmail.com>
2023-03-02 10:06:16 +0530

    Initial commit

---
+++ second.js
@@ -0,0 +1,9 @@
+var a=10;
+var b=20
+// document.write(a+b);
+console.log(a+b);
+// function displayDate() {
+//   document.getElementById("demo").innerHTML = Date();
+// }
+// var abcd=document.getElementById('d')
+// abcd[0].style.background="yellow";
